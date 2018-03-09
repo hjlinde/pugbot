95aaeb8a7707cfee26e658f9e470c9e93831531c
Jurgen Linde <hjlinde@me.com>
2018-03-09 19:53:55 +0200

    client.channels.get

--- src/services/PugQueue.js
+++ src/services/PugQueue.js
@@ -41,7 +41,7 @@ export class PugQueue {
       mentions += `${member}\n`
     })
 
-    const channel = guild.channels.get(process.env.PUGS_ANNOUNCEMENTCHANNEL)
+    const channel = client.channels.get(process.env.PUGS_ANNOUNCEMENTCHANNEL)
     	channel.send('The players for this game are:\n${mentions}')
     // Empty queue
     this.queue.length = 0
